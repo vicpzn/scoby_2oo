1dc13b4e346486ec57a9473b5426a1754550a3d4
Victorien Provenzano <victorien.provenzano@icloud.com>
2021-01-11 14:05:54 +0100

    mapbox

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,26 @@
 import React from "react";
+import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+
+const Map = ReactMapboxGl({
+  accessToken: "process.env.REACT_APP_MAPBOX_TOKEN",
+});
 
 const Home = (props) => {
   // Implement react map box here.
   return (
     <div>
-      <h1>MAPBOX MAP HERE</h1>
+      <Map
+        style="mapbox://styles/mapbox/streets-v9"
+        containerStyle={{
+          height: "100vh",
+          width: "100vw",
+        }}
+      >
+        <Layer type="symbol" id="marker" layout={{ "icon-image": "marker-15" }}>
+          <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
+        </Layer>
+      </Map>
       <p>On home /</p>
     </div>
   );
