c39d2c752a1fc61ba5b2fb4e5919bcf948b2ee18
Victorien Provenzano <victorien.provenzano@icloud.com>
2021-01-12 10:06:11 +0100

    added cards on the map

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactMapboxGl, { Layer, Feature, Marker } from "react-mapbox-gl";
+import ReactMapboxGl, { Popup, Marker } from "react-mapbox-gl";
 import apiHandler from "./../api/apiHandler";
 import "mapbox-gl/dist/mapbox-gl.css";
 
@@ -40,12 +40,34 @@ class Home extends React.Component {
               anchor="bottom"
               style={{ backgroundColor: "white", border: "1px solid black" }}
               onClick={() => {
-                this.setState({ selectedItem: item });
+                if (this.state.selectedItem === null) {
+                  this.setState({ selectedItem: item });
+                } else {
+                  this.setState({ selectedItem: null });
+                }
               }}
             >
               <img style={{ width: "50px" }} src={item.image} alt={item.name} />
             </Marker>
           ))}
+          {this.state.selectedItem !== null && (
+            <Popup
+              coordinates={this.state.selectedItem.location.coordinates}
+              offset={{
+                "bottom-left": [12, -38],
+                bottom: [0, -38],
+                "bottom-right": [-12, -38],
+              }}
+            >
+              <p>{this.state.selectedItem.name}</p>
+              <p>
+                Quantity: {this.state.selectedItem.quantity} |{" "}
+                {this.state.selectedItem.category}
+                <p>{this.state.selectedItem.description}</p>
+                <p>{this.state.selectedItem.address}</p>
+              </p>
+            </Popup>
+          )}
         </Map>
       </div>
     );
